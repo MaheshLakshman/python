fbb590baab7cdc7a5e10634095d06af9f4e1933f
mahesh l <mahesh.l@ivorytechnologies.com>
2024-11-09 21:52:44 +0530

    python demo

---
+++ Variable.py
@@ -0,0 +1,49 @@
+
+if 5 > 3:
+    print('5 greater than 3')
+if 3 < 6:
+    print('3 less than 6')
+
+
+#this is comment line
+
+''' Hai this is a multiline comment
+    from python
+'''
+
+""" Hai we can comment 
+like this
+"""
+
+x = 5
+
+print(x)
+
+x = y = 0
+
+print(' x and y', x, y)
+
+print(' x + y', x + y)
+
+print('type of x', type(x))
+
+print('type of 1', str(1))
+
+print('type of 1', int(1))
+
+print('type of 6', float(6))
+
+x, y, z = 1, 2, 3
+
+print('multple value assign in single line', x, y, z)
+
+
+x = y = z = 0
+
+print('single value in multiple variable ', x, y, z)
+
+a, b, c = ['a', 'b', 'c']
+
+print(a, b, c)
+
+
