aaa411e730ec039c12f2a8e7b310fb14e9bfd0d3
mahesh l <mahesh.l@ivorytechnologies.com>
2024-12-14 09:47:17 +0530

    Data type

---
+++ DataType.py
@@ -0,0 +1,35 @@
+print('Data Type In Python')
+
+x = 321
+print(type(x))
+
+a = 2.6
+print(type(a))
+
+b = "hello"
+print(type(b))
+
+c = 6j
+print(type(c))
+
+d = False
+print(type(d))
+
+e = True
+print(type(e))
+
+f = [1, "Sdsdsd", 'd']
+print(type(f))
+
+g = (1, 'gg', 69)
+print(type(g))
+
+h = {'dfdf', 58}
+print(type(h))
+
+i = {'y', 'u'}
+print(type(i))
+
+j = {'apple':30, 'banan':90}
+print(type(j))
+
